837906cca4aa079a65f04cd119799d1e64c293d7
Ben Miriello <benmiriello@gmail.com>
2019-11-06 04:47:53 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -29,6 +29,7 @@ def find_greater_pair(src)
       uber_array << src[row_index][0]
     elsif src[row_index][0] < src[row_index][1]
       uber_array << src[row_index][1]
+    end
     row_index += 1
   end #=> nil
   uber_array
