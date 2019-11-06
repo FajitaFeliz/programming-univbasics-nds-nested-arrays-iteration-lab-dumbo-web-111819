bb06f000d61e0231494a704b485d014ebe84f751
Ben Miriello <benmiriello@gmail.com>
2019-11-06 04:53:01 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -52,8 +52,8 @@ def total_even_pairs(src)
       if src[row_index][0] % 2 = 0 && src[row_index][1] % 2 = 0
         total += src[row_index][0]
         total += src[row_index][1]
-        element_index += 1
       end
+      element_index += 1
     end
     row_index += 1
   end
